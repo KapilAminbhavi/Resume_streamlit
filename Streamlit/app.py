a79e5f2a9ad5681809053789c4650fbbc793f6d5
KapilAminbhavi <44188469+KapilAminbhavi@users.noreply.github.com>
2024-08-06 11:15:43 +0530

    Update app.py

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -64,6 +64,7 @@ Keyword Match: [Percentage]
 Calculation for above Percentage: Break down the percentage calculation by aggregating all 4, i.e, Semantic Similarity, Contextual Relevance, Synonym Matching and Keyword Density and then provide percentage. 
 Skill Experience Match: [Percentage]
 Calculation for above Percentage: Break down the percentage calculation by aggregating all 4, i.e Experience Relevance, Skill level alignment, recency of experience, skill utilization and then provide percentage.
+Prominent Skills: []
 Highlight the most prominent skills for the respective job description in just one line.
 Overall Match: [Percentage]
 
@@ -72,6 +73,7 @@ Keyword Match: [Percentage]
 Calculation for above Percentage: Break down the percentage calculation by aggregating all 4, i.e, Semantic Similarity, Contextual Relevance, Synonym Matching and Keyword Density and then provide percentage. 
 Skill Experience Match: [Percentage]
 Calculation for above Percentage: Break down the percentage calculation by aggregating all 4, i.e Experience Relevance, Skill level alignment, recency of experience, skill utilization and then provide percentage.
+Prominent Skills: []
 Highlight the most prominent skills for the respective job description in just one line.
 Overall Match: [Percentage]
 
